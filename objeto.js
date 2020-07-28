bdb56a121dfe24ee03ba7094149d24f5965cf689
davidweb182 <davidjoseuh18@gmail.com>
2020-07-27 19:51:50 -0500

    Update

---
+++ objeto.js
@@ -0,0 +1,19 @@
+const Madre = {
+    nombre: 'Leila',
+    apellido: 'Urrego',
+    edad: 47,
+    sexo: 'F',
+};
+
+const Yo = {
+    nombre: 'David',
+    apellido: 'Urrego',
+    edad: 24,
+    sexo: 'M',
+};
+
+const familyArray = [Madre, Yo];
+
+console.log(familyArray);
+
+
